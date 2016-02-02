c63a3cddf722e76e25435a3d1cd94920ac986592
Manuel Cordoba <manuel.cordoba@mcordoba-01.workdayinternal.com>
2016-02-02 09:19:57 -0800

    added parameter to method calls for convention

--- exercises/src/test/scala/com/cordoba/org/exercises/datastructures/DeQueueTest.scala
+++ exercises/src/test/scala/com/cordoba/org/exercises/datastructures/DeQueueTest.scala
@@ -13,29 +13,29 @@ class DeQueueTest extends FunSuite with BeforeAndAfter {
   test(" add some and print") {
 
     var queue = new DeQueue[Integer]
-    queue.addFirst(1).addFirst(2).addFirst(3).printQueue
+    queue.addFirst(1).addFirst(2).addFirst(3).printQueue()
 
     queue = new DeQueue[Integer]
-    queue.addFirst(1).addLast(4).addFirst(2).addLast(6).printQueue
+    queue.addFirst(1).addLast(4).addFirst(2).addLast(6).printQueue()
 
 
     queue = new DeQueue[Integer]
-    queue.addFirst(1).addLast(4).addFirst(2).addFirst(60).addLast(6).addLast(22).printQueue
+    queue.addFirst(1).addLast(4).addFirst(2).addFirst(60).addLast(6).addLast(22).printQueue()
 
-    queue.removeFirst
-    queue.removeLast
-    queue.printQueue
+    queue.removeFirst()
+    queue.removeLast()
+    queue.printQueue()
 
-    queue.removeLast
-    queue.removeLast
-    queue.printQueue
+    queue.removeLast()
+    queue.removeLast()
+    queue.printQueue()
 
-    queue.removeFirst
-    queue.removeFirst
-    queue.printQueue
+    queue.removeFirst()
+    queue.removeFirst()
+    queue.printQueue()
 
-    queue.removeFirst
-    queue.removeLast
-    queue.printQueue
+    queue.removeFirst()
+    queue.removeLast()
+    queue.printQueue()
   }
 }
